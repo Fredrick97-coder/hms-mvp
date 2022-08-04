6781a4a64769268f204a42ebfb0fa62b5c51dcd5
Fredrick97-coder <fredrickotabil97@gmail.com>
2022-08-04 14:07:42 +0000

    edits

--- client/src/pages/login/login-component.tsx
+++ client/src/pages/login/login-component.tsx
@@ -44,56 +44,61 @@ export const LoginComponent: React.FC = () => {
             isSubmitting,
           }) => (
             <form onSubmit={handleSubmit} className="form">
-              <h5 style={{ textAlign: 'center' }}>
-                click on the button to enter email
-              </h5>
-              <>
-                {!isNext === true ? (
-                  <div className="input__wrapper">
-                    <div
-                      className="rounded__icon"
-                      onClick={() => setEmailActive((prev) => !prev)}
-                    >
-                      <MdEmail size={25} color="#575ce5" />
-                    </div>
-                    {emailActive && (
-                      <TextField
-                        name="email"
-                        type="email"
-                        value={values.email}
-                        placeholder="Enter Email"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        touched={touched}
-                        errors={errors}
-                        className="form__input"
-                      />
-                    )}
+              {!isNext === true ? (
+                <div className="input__wrapper">
+                  {!emailActive && (
+                    <h5 style={{ textAlign: 'center' }}>
+                      click on the button to enter email
+                    </h5>
+                  )}
+                  <div
+                    className="rounded__icon"
+                    onClick={() => setEmailActive((prev) => !prev)}
+                  >
+                    <MdEmail size={25} color="#575ce5" />
                   </div>
-                ) : (
-                  <div className="input__wrapper">
-                    <div
-                      className="rounded__icon"
-                      onClick={() => setPasswordActive((prev) => !prev)}
-                    >
-                      <FaUserLock size={25} color="#575ce5" />
-                    </div>
-                    {passwordActive && (
-                      <TextField
-                        name="password"
-                        type="password"
-                        value={values.password}
-                        placeholder="Enter Password"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        touched={touched}
-                        errors={errors}
-                        className="form__input"
-                      />
-                    )}
+                  {emailActive && (
+                    <TextField
+                      name="email"
+                      type="email"
+                      value={values.email}
+                      placeholder="Enter Email"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      touched={touched}
+                      errors={errors}
+                      className="form__input"
+                    />
+                  )}
+                </div>
+              ) : (
+                <div className="input__wrapper">
+                  {!passwordActive && (
+                    <h5 style={{ textAlign: 'center' }}>
+                      click on the button to enter password
+                    </h5>
+                  )}
+                  <div
+                    className="rounded__icon"
+                    onClick={() => setPasswordActive((prev) => !prev)}
+                  >
+                    <FaUserLock size={25} color="#575ce5" />
                   </div>
-                )}
-              </>
+                  {passwordActive && (
+                    <TextField
+                      name="password"
+                      type="password"
+                      value={values.password}
+                      placeholder="Enter Password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      touched={touched}
+                      errors={errors}
+                      className="form__input"
+                    />
+                  )}
+                </div>
+              )}
               {isNext === true ? (
                 <button type="submit" className="btn">
                   Submit
